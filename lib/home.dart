f1f166f1cba9d0930c12a8656b1715fe330ad67c
yukimiyake0607 <yukimiyake38@gmail.com>
2024-07-20 00:29:06 +0900

    add: counter_providerを表示し、状態を監視

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,33 @@
+import 'package:counter_riverpod/counter_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('CounterApp'),
       ),
       body: Center(
-        child: Text('Hello World!'),
+        child: Text(
+          '$counter',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {
+          ref.read(counterProvider.notifier).increment();
+        },
+        child: Icon(
+          Icons.add,
+        ),
       ),
     );
   }
